7a386b5e904c7621a4353854052254c6790f25d4
VAVolkov <green_stone@ukr.net>
2022-08-04 12:30:55 +0300

    Site menu on gulp

---
+++ src/js/about.js
@@ -0,0 +1,1 @@
+let text = 'If you’ve been to one of our restaurants, you’ve seen – and tasted – what keeps our customers coming back for more. Perfect materials and freshly baked food, delicious Lambda cakes, muffins, and gourmet coffees make us hard to resist! Stop in today and check us out!';
